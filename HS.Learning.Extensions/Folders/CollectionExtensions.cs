bd8650acf561e3281c7f8ac32e6b9321bfe9d3f0
Mason Hardy <hardymasonj@gmail.com>
2022-04-02 23:36:05 -0500

    Added AddOrUpdate method

---
+++ HS.Learning.Extensions/Folders/CollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HS.Learning.Extensions.Folders
+{
+    public static class CollectionExtensions
+    {
+        public static void AddOrUpdate<TKey, TValue>(this IDictionary<TKey, TValue> collection, TKey key, TValue value)
+        {
+            if (collection.ContainsKey(key)) 
+            {
+                collection[key] = value;
+                return;
+            }
+            collection.Add(key, value);
+        }
+    }
+}
