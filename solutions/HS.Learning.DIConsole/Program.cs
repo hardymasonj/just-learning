509fe9458162ec1cf79c88000a9c61929617dbe0
Mason Hardy <hardymasonj@gmail.com>
2022-04-16 08:42:56 -0500

    Added a class project for DI in a Console Application

---
+++ solutions/HS.Learning.DIConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HS.Learning.DIConsole.Configuration;
+using HS.Learning.DILibrary.Models;
+using HS.Learning.DILibrary.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Console.WriteLine("Hello, World!");
+
+IHost host = Host.CreateDefaultBuilder().ConfigureServices(
+    (_, services) => services.AddScoped<IUrlStore, UrlStoreModel>()
+                                            .AddScoped<IAPIWrapper, APIWrapper>()
+    ).Build();
+var apiWrapper = host.Services.GetService<IAPIWrapper>();
+Console.WriteLine(apiWrapper.CalendarAPI);
---
+++ solutions/HS.Learning.DILibrary/Services/APIWrapper.cs
@@ -0,0 +1,18 @@
+﻿using HS.Learning.DILibrary.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HS.Learning.DILibrary.Models
+{
+    public class APIWrapper : IAPIWrapper
+    {
+        private readonly IUrlStore _urlStore;
+
+        public APIWrapper(IUrlStore urlStore)
+        {
+            this._urlStore = urlStore;
+        }
+        public string CalendarAPI => _urlStore.CalendarAPI;
+    }
+}
